0c16ec3075d5bc1a4b99bf423873268ad5b79127
StavrosChryselis <StavrosChryselis@users.noreply.github.com>
2017-03-06 16:26:13 +0200

    Create ENCONDIN.cpp

---
+++ ENCONDIN.cpp
@@ -0,0 +1,44 @@
+/*
+****************************************************************
+****************************************************************
+-> Coded by Stavros Chryselis				   
+-> Visit my github for more solved problems over multiple sites 
+-> https://github.com/StavrosChryselis			  
+-> Feel free to email me at stavrikios@gmail.com	
+****************************************************************
+****************************************************************
+*/
+
+#include <stdio.h>
+#include <string.h>
+ 
+int main()
+{
+	char str[10001], ch;
+	int i, j, k, len;
+	while(gets(str))
+	{
+		len = strlen(str);
+		for(i=0; i<len;)
+		{
+			if(str[i]!=str[i+1])
+			{
+				printf("1");
+				for(j=i; j<len && str[j]!=str[j+1]; j++, i++)
+				{
+					if(str[j]=='1') printf("11");
+					else printf("%c", str[j]);
+				}
+				printf("1");
+			}
+			else
+			{
+				ch = str[i];
+				for(j=i, k=0; j<len && k<9 && str[j]==ch; j++, i++, k++);
+				printf("%d%c", k, ch);
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+} 
